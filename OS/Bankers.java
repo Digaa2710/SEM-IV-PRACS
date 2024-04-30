6827602adf9d1d199d743668b21b400e37ea4b15
Digaa2710 <143233888+Digaa2710@users.noreply.github.com>
2024-04-30 14:54:14 +0530

    Update Bankers.java

--- OS/Bankers.java
+++ OS/Bankers.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Bankers {
+public class Main {
     public static void main(String args[]) {
 
         Scanner sc = new Scanner(System.in);
@@ -12,11 +12,14 @@ public class Bankers {
         System.out.println("Enter resources:");
         resources = sc.nextInt();
         int[][] allocation = new int[processes][resources];
-        int[][] max = new int[processes][resources];
-        int[][] need = new int[processes][resources];
-        int[] available = new int[resources];
-        boolean[] visited = new boolean[processes];
-        int[] allocated_processes = new int[processes];
+        int [][] max=new int[processes][resources];
+        int [][] need=new int [processes][resources];
+        int [] available=new int [resources];
+        boolean [] visited=new boolean [processes];
+        for (int i = 0; i < processes; i++) {
+                visited[i] = false;
+        }
+        int [] allocated_processes=new int [processes];
         System.out.println("Enter allocated resources:");
         for (int i = 0; i < processes; i++) {
             for (int j = 0; j < resources; j++) {
@@ -27,71 +30,64 @@ public class Bankers {
         System.out.println("Allocates resources are:");
         for (int i = 0; i < processes; i++) {
             for (int j = 0; j < resources; j++) {
-                System.out.print(allocation[i][j] + " ");
+                System.out.print(allocation[i][j]+" ");
             }
             System.out.println(" ");
         }
         System.out.println("Enter maximum resources");
-        for (int i = 0; i < processes; i++) {
-            for (int j = 0; j < resources; j++) {
-                max[i][j] = sc.nextInt();
+        for(int i=0;i<processes;i++){
+            for(int j=0;j<resources;j++){
+                max[i][j]=sc.nextInt();
             }
         }
         System.out.println("Need is:");
-        for (int i = 0; i < processes; i++) {
-            for (int j = 0; j < resources; j++) {
-                need[i][j] = max[i][j] - allocation[i][j];
-                System.out.print(need[i][j] + " ");
+        for(int i=0;i<processes;i++){
+            for(int j=0;j<resources;j++){
+                need[i][j]=max[i][j]-allocation[i][j];
+                System.out.print(need[i][j]+" ");
             }
             System.out.println(" ");
         }
         System.out.println("Enter available:");
-        for (int i = 0; i < resources; i++) {
-            available[i] = sc.nextInt();
-            System.out.print(available[i] + " ");
+        for(int i=0;i<resources;i++){
+            available[i]=sc.nextInt();
+            System.out.print(available[i]+" ");
         }
         System.out.println(" ");
-        int k = 0;
-        // for checking resources
-        for (int i = 0; i < processes; i++) {
-            canAllocate = true;
-            visited[i] = true;
-            for (int j = 0; j < resources; j++) {
-                if (need[i][j] > available[j]) {
-                    canAllocate = false;
-                    visited[i] = false;
+        int k=0;
+        //for checking resources
+        while(k<processes){
+        for(int i=0;i<processes;i++){
+            canAllocate=true;
+            
+            for(int j=0;j<resources;j++){
+                if(!visited[i] && need[i][j]>available[j] ){
+                    canAllocate=false;
+                    visited[i]=false;
                     break;
                 }
             }
-            if (canAllocate) {
-                for (int j = 0; j < resources; j++) {
-                    available[j] += allocation[i][j];
-
+            if(!visited[i] && canAllocate){
+                for(int j=0;j<resources;j++){
+                    available[j]+=allocation[i][j];
+                   
                 }
-                allocated_processes[k] = i;
-                k++;
-
+                 
+                 allocated_processes[k]=i;
+                 k++;
+                visited[i]=true;
+                
             }
         }
-        for (int i = 0; i < processes; i++) {
-            if (!visited[i]) {
-                for (int j = 0; j < resources; j++) {
-
-                    available[j] += allocation[i][j];
-
-                }
-                allocated_processes[k] = i;
-                k++;
-            }
-        }
-        for (int i = 0; i < resources; i++) {
-            System.out.print(available[i] + " ");
-
+    }
+        for(int i=0;i<resources;i++){
+            System.out.print(available[i]+" ");
+          
         }
         System.out.println(" ");
-        for (int i = 0; i < k; i++) {
-            System.out.println("Processes will be allocated in following manner:" + allocated_processes[i] + " ");
+        for(int i=0;i<k;i++){
+              System.out.println("Processes will be allocated in following manner:"+allocated_processes[i]+" ");
         }
-
+        
     }
 }
