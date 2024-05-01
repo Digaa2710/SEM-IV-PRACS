1d087eb7c95751e26b15b50137c6a443877cf5d5
Digaa2710 <143233888+Digaa2710@users.noreply.github.com>
2024-05-01 14:37:53 +0530

    Prim.java

--- DAA/Prim.java
+++ DAA/Prim.java
@@ -33,8 +33,8 @@ public class Prim {
 
             for (int i = 0; i < n; i++) {
                 if (!visited[index] && map[index][i] != 0) {
-                    if (distance[i] >= distance[index] + map[index][i]) {
-                        distance[i] = distance[index] + map[index][i];
+                    if (distance[i] >= distance[index]) {
+                        distance[i] = distance[index] ;
                         parent[i] = index;
                         min_arr[i] = distance[i];
 
