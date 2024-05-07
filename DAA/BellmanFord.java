4681140a9cb4a06b5f9447b335d3e7d2a59881d8
Divya Gandhi <143233888+Digaa2710@users.noreply.github.com>
2024-05-07 17:35:46 +0530

    Update BellmanFord.java

--- DAA/BellmanFord.java
+++ DAA/BellmanFord.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 public class BellmanFord {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         int source = 0;
         System.out.println("Enter number of vertices:");
@@ -17,42 +16,23 @@ public class BellmanFord {
             }
             System.out.println(" ");
         }
-        boolean[] visited = new boolean[n];
-        for (int i = 0; i < n; i++) {
-            visited[i] = false;
-        }
         distance[0] = 0;
         for (int i = 1; i < n; i++) {
             distance[i] = 100;
             parent[i] = -9999;
         }
-
         int k = 0;
-        int index = 0;
-        int[] min_arr = new int[n];
-        while (k < n) {
-            int min = 1000;
-
+        while (k < n - 1) {
             for (int i = 0; i < n; i++) {
-                if (!visited[index] && map[index][i] != 0) {
-                    if (distance[i] >= distance[index] + map[index][i]) {
-                        distance[i] = distance[index] + map[index][i];
-                        parent[i] = index;
-                        min_arr[i] = distance[i];
-
+                for (int j = 0; j < n; j++) {
+                    if (map[j][i] != 0) {
+                        if (distance[i] >= distance[j] + map[j][i]) {
+                            distance[i] = distance[j] + map[j][i];
+                            parent[i] = j;
+                        }
                     }
-
-                }
-
-            }
-            visited[index] = true;
-            for (int i = 0; i < n; i++) {
-                if (!visited[i] && min > min_arr[i]) {
-                    min = min_arr[i];
-                    index = i;
                 }
             }
-
             k++;
         }
         // to check further relaxation is possible or not
