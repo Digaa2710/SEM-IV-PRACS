0de2219fdd215da4428f681bf7e9560117399569
Divya Gandhi <143233888+Digaa2710@users.noreply.github.com>
2024-05-08 17:24:42 +0530

    SelectionSort

---
+++ DAA/SelectionSort.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<time.h>
+
+int selection_sort(int array[],int n ){
+    int key;
+    int j;
+    for(int i=0;i<n;i++){
+        key=array[i];
+        j=i-1;
+        while(j>=0 && array[j]>key){
+            array[j+1]=array[j];
+            j--;
+        }
+        array[j+1]=key;
+    }
+}
+int main(){
+  int  n=1000;
+  int array[1000];
+    clock_t t1,t2,t3;
+    t1=clock();
+    selection_sort(array,n);
+    t2=clock();
+    t3=t2-t1;
+    printf("Best case:%d\n",t3);
+    int j=0;
+    for(int i=999;i>=0;i--){
+        array[i]=j;
+        j++;
+    }
+    t1=clock();
+    selection_sort(array,n);
+    t2=clock();
+    t3=t2-t1;
+    printf("Worst case:%d\n",t3);
+    
+    for(int i=0;i<n;i++){
+        array[i]=rand();
+    }
+     t1=clock();
+    selection_sort(array,n);
+    t2=clock();
+    t3=t2-t1;
+    printf("Average case:%d\n",t3);
+}
