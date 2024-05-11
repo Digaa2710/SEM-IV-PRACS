65fb46c5d1a7d25b8cb036d948fb603dd00d4c95
Divya Gandhi <143233888+Digaa2710@users.noreply.github.com>
2024-05-11 22:57:45 +0530

    NextFit

--- OS/NextFit.c
+++ OS/NextFit.c
@@ -2,19 +2,20 @@
 int usedProcesses[100]={0,0,0,0,0};
 int diff;
 int main(){
-    int blockSize[]={100,500,300,200,600};
-    int processSize[]={212,417,212,426};
+    int blockSize[]={125,500,200,300,600};
+    int processSize[]={450,417,112,290,90};
     int lastBlock=0;
-    for(int i=0;i<4;i++){
+    for(int i=0;i<5;i++){
         int index=-1;
-        for(int j=lastBlock;j<5;j++){
+        for(int j=(lastBlock+1)%5;j!=lastBlock;j=(j+1)%5){
             if(usedProcesses[j]==0 && blockSize[j]>=processSize[i]){
                     index=j;
                     lastBlock=j;
+                    
                     break;
                 }
             }
-            lastBlock=0;
+            
         
         if(index!=-1){
             printf("Pocess %d goes to block %d\n",processSize[i],blockSize[index]);
