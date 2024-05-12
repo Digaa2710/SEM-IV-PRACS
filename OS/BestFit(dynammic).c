ae7b30abeb891f3258cf6e286b262d58829dc5a2
Divya Gandhi <143233888+Digaa2710@users.noreply.github.com>
2024-05-12 18:37:06 +0530

    Add files via upload

---
+++ OS/BestFit(dynammic).c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int remaining[100];
+int main(){
+    int blockSize[]={150,350};
+    for(int i=0;i<2;i++){
+        remaining[i]=blockSize[i];
+    }
+    int diff;
+    int processSize[]={300,25,125,50};
+    for(int i=0;i<4;i++){
+        int index=-1;
+        int min=1000;
+        
+        for(int j=0;j<2;j++){
+            
+            diff=remaining[j]-processSize[i];
+            if(remaining[j]>=processSize[i] && min>diff ){
+                min=diff;
+                index=j;
+            }
+        }
+        if(index!=-1){
+            printf("Process %d goes to block %d\n",processSize[i],blockSize[index]);
+            remaining[index]-=processSize[i];
+        }
+    } 
+}
