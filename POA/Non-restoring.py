59a1f06ac5a3bef8429aae331ef43dd36fa204f1
Divya Gandhi <143233888+Digaa2710@users.noreply.github.com>
2024-05-15 07:28:35 +0530

    Non-restoring.py for any number of bits

--- POA/Non-restoring.py
+++ POA/Non-restoring.py
@@ -1,140 +1,97 @@
-def add(a, b, carry):
-    answer = [0, 0, 0, 0,0]
-    j = len(a) - 1
-    while j >= 0:
-        bit = a[j] + b[j] + carry
-        if bit == 0:
-            carry = 0
-            answer[j] = 0
-        elif bit == 1:
-            carry = 0
-            answer[j] = 1
-        elif bit == 2:
-            carry = 1
-            answer[j] = 0
-        else:
-            carry = 1
-            answer[j] = 1
-        j -= 1
-    if carry == 1:  
-        answer.insert(0, 1)
-    return answer
-
-def one_complement(M):
-    new_M = []
-    for i in range(5):
-        if M[i] == 0:
-            new_M.append(1)
-        else:
+def binary_decimal(accum,Q):
+    i=len(accum)-1
+    sum=0
+    power=0
+    while i>=0:
+        sum+=accum[i]*(2**power)
+        power+=1
+        i-=1
+    print("reminder is :",sum)   
+    i=len(Q)-1
+    sum=0
+    power=0
+    while i>=0:
+        sum+=Q[i]*(2**power)
+        power+=1
+        i-=1
+    print("quotient is :",sum)   
+def add(a,b,carry,n):
+    ans=[0]*(len(a))
+    j=len(a)-1
+    while j>=0:
+        bit=a[j]+b[j]+carry
+        if bit==0:
+            carry=0
+            ans[j]=0
+        elif bit==1:
+            carry=0
+            ans[j]=1
+        elif bit==2:
+            carry=1
+            ans[j]=0
+        elif bit==3:
+            carry=1
+            ans[j]=1
+        j=j-1
+    return ans
+def one_complement(M,n):
+    new_M=[]
+    for i in range(n+1):
+        if M[i]==1:
             new_M.append(0)
+        elif M[i]==0:
+            new_M.append(1)
+   
     return new_M
 
-def two_complement(M):
-    ans = [0, 0, 0, 0, 0]
-    new_M = one_complement(M)
-    M_complement = add(new_M, ans, 1)
-    return M_complement
-
-def Non_restoring(Q, M, accum):
- k=4
- while k>0:
-    digit=accum[0]
-    accum = accum[1:] + [Q[0]]
-   
-    print("Accumulator after shifting:", accum[1:])
-    if digit == 0:
-        M_complement = two_complement(M)
-        answer = add(accum, M_complement, 0)
-        if len(answer)==6:
-            answer=answer[1:]
-        accum=answer
-        print("Answer of the addition:",answer)
-        print("Carry:",accum[0])
-        if answer[0] == 0:
-            Q = Q[1:] + [1]
-        else:
-            Q = Q[1:] + [0]
-        
-    elif digit == 1:
-        
-        answer = add(accum, M, 0)
-        if len(answer)==6:
-            answer=answer[1:]
-        accum=answer
-        print("Answer of the addition is :",answer)
-        print("Carry:",accum[0])
-        if answer[0] == 0:
-            Q = Q[1:] + [1]
-            
-        else:
-            Q = Q[1:] + [0] 
-    print("Q after every right shift",Q)
-    print(" ")
-    k-=1
- if answer[0]==1:
-     answer=add(answer,M,0)
-     print("Quotient:",Q)
-     print("Reminder:",answer[2:])
-     Quotient=Q
-     Reminder=answer[2:]
-     
- else:
-     print("Quotient:",Q)
-     print("Reminder:",answer[1:])
-     Quotient=Q
-     Reminder=answer[1:]
- print(" ")
- binary_decimal(Quotient,Reminder)
-def binary_decimal(Q, R):
-    i = len(Q) - 1
-    decimal_sum = 0
-    power = 0
-    while i >= 0:
-        decimal_sum += Q[i] * (2 ** power)
-        power += 1
-        i -= 1
-    print("Quotient in decimal:", decimal_sum)
-
-    i = len(R) - 1
-    decimal_sum = 0
-    power = 0
-    while i >= 0:
-        decimal_sum += R[i] * (2 ** power)
-        power += 1
-        i -= 1
-    print("Reminder in decimal:", decimal_sum)
+def two_complement(M,n):
+    M_ocomplement=one_complement(M,n)
+    answer=[0]*(n+1)
+    M_tcomplement=add(M_ocomplement,answer,1,n)
+    return M_tcomplement
     
- 
- 
-
-if __name__ == "__main__":
-    # User input default 4
-    Q = int(input("Enter the dividend(decimal):"))
-    M = int(input("Enter the divisor(decimal):"))
-    print(" ")
-    l1 = []
-    l2 = []
-    accum = [0, 0, 0, 0, 0]
-    while Q != 0:
-        l1.append(Q % 2)
-        Q = Q // 2 
-    while len(l1) < 4:
-        l1.append(0)
-
-    l1.reverse()
-    print("Binary representation of Q:", l1)
-
-    while M != 0:
-        l2.append(M % 2)
-        M = M // 2 
-    while len(l2) < 4:
-        l2.append(0)
-
-    l2.reverse()
-    l2=[0]+l2
-    print("Binary representation of M:", l2[1:])
-  
-    print("Accumulator:", accum)
-
-    Non_restoring(l1, l2, accum)
+def Non_restoring(Q,M,accum,n):
+    for i in range(n):
+        accum=accum[1:]+[Q[0]]
+       
+        if accum[0]==0:
+            M_tcomplement=two_complement(M,n)
+            accum=add(accum,M_tcomplement,0,n)
+            if accum[0]==1:
+                Q=Q[1:]+[0]
+                
+            elif accum[0]==0:
+                Q=Q[1:]+[1]
+        elif accum[0]==1:
+               
+                accum=add(accum,M,0,n)
+                if accum[0]==1:
+                    Q=Q[1:]+[0]
+                elif accum[0]==0:
+                    Q=Q[1:]+[1]
+    if accum[0]==1:
+        accum=add(accum,M,0,n)
+    print(accum)
+    print(Q)
+    binary_decimal(accum,Q)
     
+Q=int(input("Enter dividend(decimal):"))
+M=int(input("Enter divisor(decimal):"))
+n=int(input("Enter highest number of bits of dividend:"))
+l1=[]
+l2=[]
+accum=[0]*(n+1)
+while Q!=0:
+    l1.append(Q%2)
+    Q=Q//2
+
+l1.reverse()
+print(l1)
+while M!=0:
+    l2.append(M%2)
+    M=M//2
+while len(l2)<(n+1):           
+    l2.append(0)
+l2.reverse()
+print(l2)
+Non_restoring(l1,l2,accum,n)
